4ede95578c8ef8a5978a73350de6b2fd53d98140
Cory Ginsberg <cory.ginsberg1@gmail.com>
2023-06-06 11:43:52 -0500

    Fix Vercel deployment errors (?)

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ module.exports = {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, './app'),
-      components: path.resolve(__dirname, './app/components'),
+      components: path.resolve(__dirname, './app/components/**'),
       sections: path.resolve(__dirname, './app/sections/**'),
       '@styles': path.resolve(__dirname, './app/styles'),
       '@public': path.resolve(__dirname, './public/**'),
