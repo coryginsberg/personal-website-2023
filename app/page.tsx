12db43d8408c2795fb72dd7837313a8fb7ff1af7
Cory Ginsberg <cory.ginsberg1@gmail.com>
2023-06-06 13:45:56 -0500

    nit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Hero from './sections/Hero';
 import {Projects} from './sections/Projects';
 import styles from './styles/page.module.scss';
 
+import React from 'react';
+
 export default function Home() {
   return (
     <div className={styles.main}>
